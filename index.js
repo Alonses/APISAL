7e4927688365e5071f2e4308660467967c69fc61
Gustavo <gusbra21@outlook.com>
2022-08-02 20:26:56 -0300

    Novas chamadas

--- index.js
+++ index.js
@@ -2,24 +2,78 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
-const cantadas = require("./src/cantadas/cantadas.json")
+const cantadas = require("./src/base/cantadas.json")
+const jailson = require("./src/base/jailson.json")
+const rasputia = require("./src/base/rasputia.json")
+
 let retiradas = []
+let caso = 0 // Redefine o array de retiradas caso altere o valor
 
 app.get("/cantadas", (req, res) => {
 
-    if(retiradas.length == cantadas.length) // Reinicia o vetor
-        retiradas = []
+    let json_final = {
+        nome: "Vai dar namoro",
+        foto: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjlN00fmdjOpxTAglegPf4-i7_YDQCfFDZK94TUVyORci0HVQN7-UcQezywuYMWJg1-BWn14JTFChll8LuHttNwxVIDmh0KtyFwtQOgOOOi8z54-z4kzqzLCD1MbSSc2_XnlD-9p8XSo5Wkwz9ZkdheKaDKnzuJJ1KsAHoKIaSt-c9ORDW4Fgd9m2lg/s990/rodrigo-faro-record-fase-ruim-audiencia-reproducao-grande_fixed_large.jpg",
+        texto: cantadas[escolhe_numero(cantadas, 0)]
+    }
 
-    let num
+    return res.json(json_final)
+})
 
-    do{
-        num = Math.round((cantadas.length - 1) * Math.random())
-    }while(retiradas.includes(num)) // Escolhendo uma cantada aleatória
+app.get("/jailson", (req, res) => {
     
-    retiradas.push(num)
-    return res.json(cantadas[num])
+    // Há frases do jailson e do guina aqui
+    let num = escolhe_numero(jailson, 1);
+    let nome_wbk = "Jailson Mendes";
+    let foto_wbk = "https://upload.wikimedia.org/wikipedia/pt/8/8d/Jailson_Mendes.jpg"
+    let texto_wbk = jailson[num]
+
+    // Frases do guina
+    if(jailson[num].startsWith("G|")){
+        nome_wbk = "Paulo Guina"
+        foto_wbk = "https://pbs.twimg.com/profile_images/1404555803646963712/nEtaBLBK_400x400.jpg"
+        texto_wbk = jailson[num].replace("G|", "");
+    }
+
+    let json_final = {
+        nome: nome_wbk,
+        foto: foto_wbk,
+        texto: texto_wbk
+    }
+
+    return res.json(json_final)
+});
+
+app.get("/rasputia", (req, res) => {
+    let json_final = {
+        nome: "Rasputia Latimore",
+        foto: "https://static.wikia.nocookie.net/antagonists/images/4/40/Rasputia_Latimore.jpg/revision/latest/scale-to-width-down/300?cb=20121110030016",
+        texto: rasputia[escolhe_numero(rasputia, 3)]
+    }
+
+    return res.json(json_final)
 })
 
 app.listen(port, () => {
     console.log("Servidor ligado na porta ", port);
 })
+
+function escolhe_numero(vetor_json, caso_acionado){
+    
+    let num;
+
+    // Reseta o vetor de repetidas em mudança de comando
+    if(vetor_json.length == retiradas.length || caso_acionado != caso){
+        retiradas = [];
+    }
+
+    caso = caso_acionado;
+
+    do{
+        num = Math.round((vetor_json.length - 1) * Math.random())
+    }while(retiradas.includes(num))
+
+    retiradas.push(num)
+
+    return num;
+}
