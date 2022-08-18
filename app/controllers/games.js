f96f86bca04bc1101b1055ff3ff3fa33c05eceb7
Gustavo <gustavobrandao31@outlook.com>
2022-08-18 12:35:21 -0300

    Anúncio de games

---
+++ app/controllers/games.js
@@ -0,0 +1,40 @@
+const { getGames } = require("epic-free-games")
+
+let dados = []
+
+class Games {
+    show(req, res){
+        
+        if(dados.length < 1){
+            getGames("BR", true).then(dados => {
+                dados = dados.currentGames
+                retorna_games(res)
+            })
+            .catch(err => {
+                console.log(err)
+                return res.json({status: "404"})
+            })
+        }else
+            retorna_games(res)
+    }
+}
+
+function retorna_games(res){
+
+    let array_games = []
+
+    dados.forEach(jogo => {
+        array_games.push({
+            nome: jogo.title,
+            descricao: jogo.description,
+            thumbnail: jogo.keyImages[0].url,
+            link: `https://store.epicgames.com/pt-BR/p/${jogo.catalogNs.mappings[0].pageSlug}`,
+            data_inicio: jogo.promotions.promotionalOffers[0].promotionalOffers[0].startDate.slice(5, 10),
+            data_final: jogo.promotions.promotionalOffers[0].promotionalOffers[0].endDate.slice(5, 10)
+        })
+    });
+
+    return res.json(array_games)
+}
+
+module.exports = new Games()
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const Comandos = require('../app/controllers/comandos')
 const txt_wbk = require('../app/controllers/txt_wbk')
 const Curiosidades = require('../app/controllers/curiosidades')
 const History = require('../app/controllers/history')
+const Games = require("../app/controllers/games")
 
 const routes = new Router()
 
@@ -11,5 +12,6 @@ routes.get('/comandos', Comandos.show)
 routes.get('/random', txt_wbk.show)
 routes.get('/curiosidades', Curiosidades.show)
 routes.get('/history', History.show)
+routes.get('/games', Games.show)
 
 module.exports = routes
