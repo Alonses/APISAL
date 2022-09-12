6a463fcec24794a2d0ab82092e255b2c4dc00b3c
lllggghhhaaa <lgha1423@outlook.com>
2022-09-12 17:39:20 -0300

    Muita ceira, mas muita ceira mesmo

--- src/app.js
+++ src/app.js
@@ -14,10 +14,10 @@ class App {
         this.app.use(express.json())
 
         this.app.use((req, res, next) => {
-            res.header("Access-Controll-Allow-Credentials", 'true')
-            res.header("Access-Controll-Allow-Origin", "*")
-            res.header("Access-Controll-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
-            res.header("Access-Controll-Allow-Headers", 'X-CSRF-Token, X-Requested-With')
+            res.header("Access-Control-Allow-Credentials", 'true')
+            res.header("Access-Control-Allow-Origin", "*")
+            res.header("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
+            res.header("Access-Control-Allow-Headers", 'X-CSRF-Token, X-Requested-With')
             
             this.app.use(cors({
                 origin: '*'
